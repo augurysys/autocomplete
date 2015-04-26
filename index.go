8b8318c8dadf1992032db89bf39ae63e9df0eaab
Gal Ben-Haim <gbenhaim@augury.com>
2015-04-26 10:16:03 +0300

    added UpdateDocument()

--- index.go
+++ index.go
@@ -2,6 +2,9 @@ package autocomplete
 
 import (
 	"encoding/json"
+	"fmt"
+
+	"github.com/garyburd/redigo/redis"
 )
 
 // Index indexes a document for autocomplete search
@@ -64,6 +67,36 @@ func (a *Autocomplete) RemoveDocument(index string, d Document) error {
 	}
 
 	if _, err := conn.Do("EXEC"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateDocument updates a document in the autocomplete search index
+func (a *Autocomplete) UpdateDocument(index string, d Document) error {
+	conn := a.pool.Get()
+	defer conn.Close()
+
+	docKey := key(d)
+
+	exists, err := redis.Bool(conn.Do("HEXISTS", a.prefix+":$"+index, docKey))
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		return fmt.Errorf("%s does not contain %s", a.prefix+":$"+index, docKey)
+	}
+
+	b, err := json.Marshal(d)
+	if err != nil {
+		return err
+	}
+
+	if _, err := conn.Do(
+		"HSET", a.prefix+":$"+index, docKey, string(b)); err != nil {
+
 		return err
 	}
 
